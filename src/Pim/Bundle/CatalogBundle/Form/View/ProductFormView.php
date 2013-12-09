0ece98a5712cf9c3bea1abed5f8b4dd5f2cfcd11
Antoine Guigan <antoine@akeneo.com>
2013-12-09 11:11:47 +0100

    Revert "Refactored ProductFormView"
    This reverts commit 8037d04959580c30d0feb594a8cd2d30aac0ab56.

--- src/Pim/Bundle/CatalogBundle/Form/View/ProductFormView.php
+++ src/Pim/Bundle/CatalogBundle/Form/View/ProductFormView.php
@@ -27,82 +27,71 @@ class ProductFormView
     /**
      * Returns an array of groups containing all attribute fields data
      *
-     * @param FormView $view
-     *
      * @return array
      */
     public function getGroups(FormView $view)
     {
-        $groupsIndex = array();
-        foreach ($view['values'] as $fieldView) {
-            $this->buildIndex($groupsIndex, $fieldView);
-        }
-        krsort($groupsIndex);
-
         $groups = array();
-        foreach ($groupsIndex as $groupIndex) {
-            foreach ($groupIndex as $groupId => $groupInfo) {
-                $groups[$groupId] = $this->getGroupView($groupInfo);
+        foreach ($view['values'] as $fieldView) {
+            $this->setFieldValue($groups, $fieldView);
+        }
+
+        foreach ($groups as &$group) {
+            ksort($group['orderedAttributes']);
+            foreach ($group['orderedAttributes'] as $attributes) {
+                $group['attributes'] += $attributes;
             }
+            unset($group['orderedAttributes']);
         }
 
         return $groups;
     }
 
     /**
-     * Returns the view for a group
-     *
-     * @param array $groupInfo
-     *
-     * @retur array
-     */
-    protected function getGroupView(array $groupInfo)
-    {
-        $view = array(
-            'label'      => $groupInfo['label'],
-            'attributes' => array()
-        );
-
-        ksort($groupInfo['attributes']);
-        foreach ($groupInfo['attributes'] as $attributes) {
-            $view['attributes'] += $attributes;
-        }
-
-        return $view;
-    }
-
-    /**
-     * Sets an attribute's values inside an array of indexed groups
+     * Sets an attribute's values inside an array of groups
      *
      * @param array    $groups
      * @param FormView $view
      */
-    protected function buildIndex(array &$groups, FormView $view)
+    protected function setFieldValue(array &$groups, FormView $view)
     {
-        $value = $view->vars['value'];
-        $attribute = $value->getAttribute();
+        $attribute = $this->getAttribute($view);
         $group = $attribute->getVirtualGroup();
         $groupId = $group->getId();
-        $groupOrder = $group->getSortOrder();
-        $attributeOrder = $attribute->getSortOrder();
-        if (!isset($groups[$groupOrder][$groupId]['attributes'][$attributeOrder])) {
-            if (!isset($groups[$groupOrder][$groupId])) {
-                if (!isset($groups[$groupOrder])) {
-                    $groups[$groupOrder] = array();
-                }
-                $groups[$groupOrder][$groupId] = array(
-                    'label'      => $group->getLabel(),
-                    'attributes' => array()
+        $order = $attribute->getSortOrder();
+        if (!isset($groups[$groupId]['orderedAttributes'][$order])) {
+            if (!isset($groups[$groupId])) {
+                $groups[$groupId] = array();
+                $groups[$groupId] = array(
+                    'label'                 => $group->getLabel(),
+                    'orderedAttributes'     => array(),
+                    'attributes'            => array()
                 );
             }
-            $groups[$groupOrder][$groupId]['attributes'][$attributeOrder] = array();
+            $groups[$groupId]['orderedAttributes'][$order] = array();
         }
-
-        $groups[$groupOrder][$groupId]['attributes'][$attributeOrder][$attribute->getId()] = $this->getAttributeView(
+        
+        $value = $view->vars['value'];
+        $groups[$groupId]['orderedAttributes'][$order][$value->getId()] = $this->getValueView(
             $value,
             $attribute,
             $view
         );
+    }
+
+    /**
+     * Returns the attribute corresponding to a view
+     *
+     * @param  FormView         $view
+     * @return ProductAttribute
+     */
+    protected function getAttribute(FormView $view)
+    {
+        foreach ($view as $valueView) {
+            if (isset($valueView['flexibleAttribute'])) {
+                return $valueView->vars['flexibleAttribute'];
+            }
+        }
     }
 
     /**
@@ -135,7 +124,7 @@ class ProductFormView
      *
      * @return array
      */
-    protected function getAttributeView(ProductValueInterface $value, ProductAttribute $attribute, FormView $view)
+    protected function getValueView(ProductValueInterface $value, ProductAttribute $attribute, FormView $view)
     {
         $attributeView = array(
             'isRemovable'        => $value->isRemovable(),
